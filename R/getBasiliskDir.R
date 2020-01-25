0541c51fee6693624207e4e1b05376c77428aa48
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-24 19:35:21 -0800

    Always set the basilisk inst dir to an external location.

--- R/getBasiliskDir.R
+++ R/getBasiliskDir.R
@@ -9,10 +9,20 @@
 #' @return String containing the path to the Anaconda instance.
 #'
 #' @details
-#' If the \code{BASILISK_USE_ANACONDA} environment variable is set,
+#' By default, Anaconda is installed to a location specified by \code{\link{getExternalDir}}.
+#' This ensures that R package build systems do not attempt to generate binaries that include the Anaconda installation;
+#' such binaries are not relocatable due to the presence of hard-coded paths, resulting in run-time failures.
+#' 
+#' If the \code{BASILISK_EXTERNAL_ANACONDA} environment variable is set to a path to an existing Anaconda installation,
 #' the function will return it directly without modification.
 #' This allows users to use their own Anaconda instances with \pkg{basilisk} but, 
 #' in turn, they are responsible for managing it.
+#'
+#' If the \code{BASILISK_USE_PACKAGE_DIR} environment variable is set to \code{"1"},
+#' the function will return a path to a location inside the \code{basilisk} installation directory.
+#' This is the ideal approach when installing from source as any Anaconda and \pkg{basilisk} re-installations are synchronized.
+#' It also ensures that any R process that can load \pkg{basilisk} will also have permissions to access the Anaconda instance,
+#' which makes life easier for sysadmins of clusters or other shared resources.
 #'
 #' If \code{assume.installed=TRUE}, this function will throw an error if the Anaconda directory does not exist.
 #' On Unix, it will also use \code{\link{system.file}} to ensure that the correct directory is recovered,
@@ -25,12 +35,13 @@
 #'
 #' @export
 getBasiliskDir <- function(assume.installed=TRUE) {
-    inst_path <- Sys.getenv("BASILISK_USE_ANACONDA")
+    inst_path <- Sys.getenv("BASILISK_EXTERNAL_ANACONDA")
     if (identical(inst_path, "")) {
-        if (isWindows() || isMacOSX()) {
+        if (!identical(Sys.getenv("BASILISK_USE_PACKAGE_DIR"), "1")) {
             inst_path <- getExternalDir()
         } else {
             if (assume.installed) {
+                # Uses the correct location if .libPaths() has been changed.
                 inst_path <- system.file(package="basilisk")
             } else {
                 # Can't use system.file(), basilisk isn't installed yet!
