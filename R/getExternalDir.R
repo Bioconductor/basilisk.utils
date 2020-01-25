3c786e66361bbfa3c43369782ef5203b2217cf84
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-01-24 19:41:28 -0800

    Switch external dir choice to avoid spaces in path.

--- R/getExternalDir.R
+++ R/getExternalDir.R
@@ -1,7 +1,6 @@
 #' Get external Anaconda installation 
 #'
-#' Define an external location for installing the Anaconda instance, 
-#' usually on Windows or MacOSX. 
+#' Define an external location for installing the Anaconda instance and \pkg{basilisk} environments.
 #'
 #' @return String containing a path to an appropriate external folder.
 #'
@@ -9,9 +8,11 @@
 #' We add the version information so that re-installation of \pkg{basilisk} will install a new instance of Anaconda.
 #' (This assumes that \pkg{basilisk} and \pkg{basilisk.utils} have synchronized version bumps.)
 #' See \code{\link{installAnaconda}} for more details on how this external location is managed.
-#' 
-#' This function will respect any setting of \code{BASILISK_EXTERNAL_DIR}, 
-#' see \code{?\link{installAnaconda}} for more details.
+#'
+#' If the \code{BASILISK_EXTERNAL_DIR} environment variable is set to some location,
+#' this will be used instead as the installation directory.
+#' Setting this variable is occasionally necessary if the default path returned by \code{\link{user_cache_dir}} has spaces;
+#' or on Windows, if the 260 character limit is exceeded after combining the default path with deeply nested Anaconda paths. 
 #'
 #' @author Aaron Lun
 #'
@@ -23,12 +24,13 @@
 #'
 #' @export
 #' @importFrom utils packageVersion
-#' @importFrom rappdirs user_data_dir
+#' @importFrom rappdirs user_cache_dir 
 getExternalDir <- function() {
     pkg.v <- as.character(packageVersion("basilisk.utils"))
     inst_path <- Sys.getenv("BASILISK_EXTERNAL_DIR")
     if (inst_path=="") {
-        user_data_dir(appname="basilisk", appauthor="me", version=pkg.v)
+        # Using "cache", not "data", as it doesn't have spaces on Mac.
+        user_cache_dir(appname="basilisk", appauthor="me", version=pkg.v)
     } else {
         file.path(inst_path, pkg.v)
     }
