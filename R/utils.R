f1f2544232a22fbb0bf9cd89e4ac3d8f3e6ad54c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-04-05 12:43:13 -0700

    Export utility to indicate destruction of old versions. Fleshed out documentation for these utilities.

--- R/utils.R
+++ R/utils.R
@@ -24,16 +24,42 @@ isMacOSX <- function() {
 #'
 #' Should we use the R system directory for installing \pkg{basilisk}'s Anaconda instance or client environments?
 #'
+#' @details
+#' The default value is \code{FALSE} to avoid problems with position-dependent code in packaged binaries.
+#' This can be changed by setting \code{BASILISK_USE_SYSTEM_DIR} environment variable to \code{"1"}.
+#' 
 #' @return Logical scalar providing an answer to the above.
 #'
 #' @author Aaron Lun
 #'
 #' @seealso
-#' \code{\link{getBasiliskDir}} and \code{\link{getEnvironmentDir}}, where these functions are used.
+#' \code{\link{getBasiliskDir}} and \code{\link{getEnvironmentDir}}, where this function is used.
 #'
 #' @export
 useSystemDir <- function() {
     identical(Sys.getenv("BASILISK_USE_SYSTEM_DIR"), "1")
+}
+
+#' Destroy old versions?
+#' 
+#' Should we destroy old installations of Anaconda from previous versions of \pkg{basilisk} or its client packages?
+#'
+#' @details
+#' The default value is \code{TRUE}, in order to save some hard drive space.
+#' This can be changed by setting \code{BASILISK_NO_DESTROY} environment variable to \code{"1"}.
+#' 
+#' @return Logical scalar providing an answer to the above.
+#'
+#' @author Aaron Lun
+#'
+#' @seealso 
+#' \code{\link{installAnaconda}}, where this function is used.
+#'
+#' \code{\link{clearObsoleteDir}}, which may be triggered by this function.
+#' 
+#' @export
+destroyOldVersions <- function() {
+    Sys.getenv("BASILISK_NO_DESTROY")!="1"
 }
 
 #' Get binary paths
