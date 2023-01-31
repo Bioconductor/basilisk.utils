c4741bffb87ece7c9b2d916368c016c5f200ed94
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-30 22:09:33 -0800

    Minor doc tweak.

--- R/isWindows.R
+++ R/isWindows.R
@@ -1,7 +1,7 @@
 #' Find the operating system or architecture.
 #'
 #' Indicate whether we are on Windows or MacOSX.
-#' For MacOSX, we can also determine if we are on an x86-64 or Arm-based architecture.
+#' For MacOSX and Linux, we can also determine if we are on an x86-64 or Arm-based architecture.
 #'
 #' @return Logical scalar indicating whether we are on the specified OS and/or architecture.
 #'
